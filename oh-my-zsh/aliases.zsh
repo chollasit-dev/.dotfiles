93440003329fe9eb380497d18cbd7f05583dcfbe
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-03-18 22:48:14 +0700

    Add batcat theme alias and remove incorrect command in clearhist

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,5 +1,5 @@
-alias cat="batcat"
-alias clearhist="history -p && history -s"
+alias cat="batcat --theme ansi"
+alias clear="clear && history -p"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
 alias t1='tree -aFpsu -L 1 '
 alias t2='tree -aFpsu -L 1 '
