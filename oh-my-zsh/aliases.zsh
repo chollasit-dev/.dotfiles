c31ffc36ba246f6e0f696d884f281b3554434e7b
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-05-21 16:19:02 +0700

    Fix Typo and add QOL shell aliases
    Remove trailing space at `tree` alias add aliases to streamline my workflow:
    
    - Git
    - Neovim

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,6 +1,11 @@
+#!/usr/bin/env sh
+
+alias c="clear && history -p"
 alias cat="batcat --theme ansi"
-alias clear="clear && history -p"
+alias g="git"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
-alias t1='tree -aFpsu -L 1 '
-alias t2='tree -aFpsu -L 1 '
+alias n="nvim"
+alias t1='tree -aFpsu -L 1'
+alias t2='tree -aFpsu -L 2'
 alias wget-staticweb='wget --convert-links --mirror --page-requisites --no-clobber --no-parent --domains'
+alias y='yazi'
